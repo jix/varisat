d9cc8e1abd1a75889393d4ab43ab4e11a3b2e287
Jannis Harder <me@jix.one>
2019-05-29 11:45:48 +0200

    Remove code duplication in DRAT writing

--- varisat/src/proof/drat.rs
+++ varisat/src/proof/drat.rs
@@ -3,20 +3,22 @@ use std::io::{self, Write};
 use varisat_formula::Lit;
 use varisat_internal_proof::ProofStep;
 
-/// Writes a proof step in DRAT format
-pub fn write_step<'s>(target: &mut impl Write, step: &'s ProofStep<'s>) -> io::Result<()> {
+/// Prepares a proof step for DRAT writing
+fn drat_step(
+    step: &ProofStep,
+    mut emit_drat_step: impl FnMut(bool, &[Lit]) -> io::Result<()>,
+) -> io::Result<()> {
     match step {
         ProofStep::AtClause { clause, .. } => {
-            write_literals(target, &clause)?;
+            emit_drat_step(true, &clause)?;
         }
         ProofStep::UnitClauses { units } => {
             for &(unit, _hash) in units.iter() {
-                write_literals(target, &[unit])?;
+                emit_drat_step(true, &[unit])?;
             }
         }
         ProofStep::DeleteClause { clause, .. } => {
-            target.write_all(b"d ")?;
-            write_literals(target, &clause[..])?;
+            emit_drat_step(false, &clause[..])?;
         }
         ProofStep::SolverVarName { .. }
         | ProofStep::UserVarName { .. }
@@ -38,41 +40,28 @@ pub fn write_step<'s>(target: &mut impl Write, step: &'s ProofStep<'s>) -> io::R
     Ok(())
 }
 
+/// Writes a proof step in DRAT format
+pub fn write_step<'s>(target: &mut impl Write, step: &'s ProofStep<'s>) -> io::Result<()> {
+    drat_step(step, |add, clause| {
+        if !add {
+            target.write_all(b"d ")?;
+        }
+        write_literals(target, &clause[..])?;
+        Ok(())
+    })
+}
+
 /// Writes a proof step in binary DRAT format
 pub fn write_binary_step<'s>(target: &mut impl Write, step: &'s ProofStep<'s>) -> io::Result<()> {
-    match step {
-        ProofStep::AtClause { clause, .. } => {
+    drat_step(step, |add, clause| {
+        if add {
             target.write_all(b"a")?;
-            write_binary_literals(target, &clause)?;
-        }
-        ProofStep::UnitClauses { units } => {
-            for &(unit, _hash) in units.iter() {
-                target.write_all(b"a")?;
-                write_binary_literals(target, &[unit])?;
-            }
-        }
-        ProofStep::DeleteClause { clause, .. } => {
+        } else {
             target.write_all(b"d")?;
-            write_binary_literals(target, &clause[..])?;
         }
-        ProofStep::SolverVarName { .. }
-        | ProofStep::UserVarName { .. }
-        | ProofStep::DeleteVar { .. }
-        | ProofStep::ChangeSamplingMode { .. }
-        | ProofStep::ChangeHashBits { .. }
-        | ProofStep::Model { .. }
-        | ProofStep::End => (),
-        ProofStep::AddClause { .. } => {
-            // TODO allow error handling here?
-            panic!("incremental clause additions not supported by DRAT proofs");
-        }
-        ProofStep::Assumptions { .. } | ProofStep::FailedAssumptions { .. } => {
-            // TODO allow error handling here?
-            panic!("assumptions not supported by DRAT proofs");
-        }
-    }
-
-    Ok(())
+        write_binary_literals(target, &clause[..])?;
+        Ok(())
+    })
 }
 
 /// Writes the literals of a clause for a step in a DRAT proof.
